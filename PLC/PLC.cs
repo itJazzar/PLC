62810b0daec5eddd4b1bf238190610536d34bace
itJazzar <cher.maksss@gmail.com>
2024-04-19 16:35:28 +0700

    Смог подключиться к ПЛК. Был косяк в проекте TIA Portal v16

--- PLC/PLC.cs
+++ PLC/PLC.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using EasyModbus;
+
 
 namespace PLC
 {
@@ -24,6 +27,7 @@ namespace PLC
         public int TimeoutConnection { get; set; }
         public PLCTypes PLCType { get; set; }
         private ModbusClient _modbusClient;
+        private CancellationTokenSource _cancellationTokenSource;
         public enum PLCTypes
         {
             Shnider,
@@ -32,63 +36,119 @@ namespace PLC
             EKF,
             Delta
         }
-        public PLC()
+        public PLC(string ipAddress = "12.12.0.10", int port = 102)
         {
-            _modbusClient = new ModbusClient(IpAddress, Port);
+            Port = port;
+            IpAddress = ipAddress;
+            _modbusClient = new ModbusClient();
+            ModbusTag modbusTag = new ModbusTag();
         }
         private void Connect()
         {
-            _modbusClient.Connect(IpAddress, Port);
-            if (_modbusClient.Connected)
+            if (_modbusClient != null && !_modbusClient.Connected)
             {
-                Console.WriteLine("Connection established.");
+                _modbusClient.Connect(IpAddress, Port);
+                if (_modbusClient.Connected)
+                {
+                    Console.WriteLine("Connection established.\n");
+                }
             }
         }
+        private async Task CheckConnection()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = _cancellationTokenSource.Token;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+
+                if (!(_modbusClient != null && _modbusClient.Connected))
+                {
+                    Connect();
+                }
+            }
+        }
+        //private async Task PingThread()
+        //{
+        //    while (!_modbusClient.Connected)
+        //    {
+        //        if (!PingDevice())
+        //        {
+        //            Console.WriteLine("Ping failed. Reconnecting...\n");
+        //            Connect();
+        //            Connect();
+        //        }
+        //        await Task.Delay(TimeSpan.FromSeconds(1));
+        //    }
+        //}
+        //private bool PingDevice()
+        //{
+        //    using (Ping ping = new Ping())
+        //    {
+        //        try
+        //        {
+        //            PingReply reply = ping.Send(IpAddress);
+        //            return reply.Status == IPStatus.Success;
+        //        }
+        //        catch (PingException)
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //}
         private void Disconnect()
         {
             _modbusClient?.Disconnect();
             if (!_modbusClient.Connected)
             {
-                Console.WriteLine("Disconnestion was done successful.");
+                Console.WriteLine("Disconnestion was done successful.\n");
             }
         }
         private bool[] ReadData()
         {
-            var result = _modbusClient.ReadDiscreteInputs(StartAddress, Quantity); //error
+            var result = _modbusClient.ReadCoils(StartAddress, Quantity); //error
             return result;
         }
-        private void SetCoils(bool value)
+        private void Write(int tag, bool value)
         {
-            _modbusClient.WriteSingleCoil(StartAddress, value);
+            _modbusClient.WriteSingleCoil(tag, value);
         }
 
-
-
-
-
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             PLC plc = new PLC();
+            plc.IpAddress = "12.12.0.50";
+            plc.Port = 502; //102 порт симёна ??; 502
             plc.PLCType = PLCTypes.Siemens;
-            plc.IpAddress = "12.12.0.11";
-            plc.Port = 102; //102 порт симёна; 502
             plc.TimeoutConnection = 1000;
             plc.StartAddress = 0;
-            plc.Quantity = 1;
+            plc.Quantity = 7;
 
             plc.Connect();
 
+            //await plc.CheckConnection();
+            //Task pingTask = plc.PingThread();
+            //await pingTask;
 
-            //plc.SetCoils(true);
+            //plc.Write(0, true);
+            //plc.Write(1, true);
+            //plc.Write(2, true);
+            //plc.Write(3, false);
+            //plc.Write(4, true);
+            //plc.Write(5, true);
+            //plc.Write(6, true);
 
             var data = plc.ReadData();
 
             for (int i = 0; i < data.Length; i++)
             {
-                Console.WriteLine($"Value of Discrete Input {i} : {data[i].ToString()}");
+                Console.WriteLine($"Value of Coil {i} : {data[i]}");
             }
 
             plc.Disconnect();
+
+
 
 
             Console.ReadKey();
@@ -103,6 +163,32 @@ namespace PLC
         //5. Автоматический опрос
         //6. Эвент изменения значения
         //7. Методы записи и чтения по данному тэгу
+
+        public event EventHandler TagChanged;
+
+
+
+
+        public enum ModbusDataType
+        {
+            Bool,         // Bit
+            Byte,         // 8-bit целое число
+            SInt,         // 8-bit знаковое целое число
+            USInt,        // 8-bit беззнаковое целое число
+            Word,         // 16-bit целое число
+            Int,          // 16-bit знаковое целое число
+            UInt,         // 16-bit беззнаковое целое число
+            DWord,        // 32-bit целое число
+            DInt,         // 32-bit знаковое целое число
+            Real,         // 32-bit число с плавающей точкой
+            UDInt,        // 32-bit беззнаковое целое число
+            Char,         // символ (8 бит)
+            Date,         // дата (16 бит)
+            Time,         // время (32 бита)
+            Time_Of_Day,  // время дня (32 бита)
+            Array,        // массив
+            Struct        // структура
+        }
 
         public ModbusTag()
         {
